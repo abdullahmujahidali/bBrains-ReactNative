12ec8aaf9dad0b69b79e36d8bdc17ccafe534149
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2021-05-09 12:01:16 +0500

    removed files

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://abdullah:abdullah@projectcluster.dsmrv.mongodb.net/bigbrains?retryWrites=true&w=majority",
-    JWT_SECRET: "bigisbrain123",
-    SENDGRID_API:"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",
-    EMAIL:"http:localhost:3000"
-}
--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    MONGOURI:process.env.MONGOURI,
-    JWT_SECRET: process.env.JWT_SEC,
-    SENDGRID_API: process.env.JWT_SEC,
-    EMAIL:process.env.EMAIL
-}
